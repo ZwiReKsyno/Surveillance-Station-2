456a1d6c7a4578d9d013600f23f10d012f9de6c7
ZwiRek <168815146+ZwiReKsyno@users.noreply.github.com>
2024-10-25 14:45:10 +0300

    Update license.sh

--- license.sh
+++ license.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
-#bash <(curl -L https://raw.githubusercontent.com/ZwiReKsyno/Surveillance-Station-2/refs/heads/main/license.sh)
-echo "0.0.0.0 utyupdate.synology.com" | sudo tee -a /etc/hosts
-echo "0.0.0.0 utyupdate.synology.com" | sudo tee -a /etc.defaults/hosts
-echo "0.0.0.0 license.synology.com" | sudo tee -a /etc/hosts
-echo "0.0.0.0 license.synology.com" | sudo tee -a /etc.defaults/hosts
-echo "0.0.0.0 update.synology.com" | sudo tee -a /etc/hosts
-echo "0.0.0.0 update.synology.com" | sudo tee -a /etc.defaults/hosts
-echo "0.0.0.0 pkgupdatetest.synology.com" | sudo tee -a /etc/hosts
-echo "0.0.0.0 pkgupdatetest.synology.com" | sudo tee -a /etc.defaults/hosts
-echo "0.0.0.0 pkgupdate.synology.com" | sudo tee -a /etc/hosts
-echo "0.0.0.0 pkgupdate.synology.com" | sudo tee -a /etc.defaults/hosts
-echo "0.0.0.0 SynologyNVR" | sudo tee -a /etc/hosts
-echo "0.0.0.0 SynologyNVR" | sudo tee -a /etc.defaults/hosts
-echo "0.0.0.0 pkgupdate7.synology.com" | sudo tee -a /etc/hosts
-echo "0.0.0.0 pkgupdate7.synology.com" | sudo tee -a /etc.defaults/hosts
-./license.sh
+
+
+# Блокировать сервер аутентификации
+if grep -q "license.synology.com" /etc/hosts; then
+  echo "already blocked license server: license.synology.com."
+else
+  echo "add block license server: license.synology.com"
+  echo "0.0.0.0 license.synology.com" | sudo tee -a /etc/hosts
+fi
+# Блокировать сервер аутентификации
+if grep -q "utyupdate.synology.com" /etc/hosts; then
+  echo "already blocked license server: utyupdate.synology.com."
+else
+  echo "add block license server: utyupdate.synology.com"
+  echo "0.0.0.0 utyupdate.synology.com" | sudo tee -a /etc/hosts
+fi
+# Блокировать сервер аутентификации
+if grep -q "update.synology.com" /etc/hosts; then
+  echo "already blocked license server: update.synology.com."
+else
+  echo "add block license server: update.synology.com"
+  echo "0.0.0.0 update.synology.com" | sudo tee -a /etc/hosts
+fi
   
